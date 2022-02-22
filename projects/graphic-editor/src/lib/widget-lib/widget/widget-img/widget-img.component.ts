23655fe76fafa5d284dc4f22148071277ab6f656
liyuhua <liyuhua@example.com>
2022-02-22 16:54:41 +0800

    update

--- projects/graphic-editor/src/lib/widget-lib/widget/widget-img/widget-img.component.ts
+++ projects/graphic-editor/src/lib/widget-lib/widget/widget-img/widget-img.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WidgetData } from '../../../model';
+import { ImgSetting } from '../../../widget-setting/settings-lib/img-setting/img-setting.component';
 import { BaseWidgetContent } from '../base-widget-content';
-
-export type ImgWidgetData = Exclude<WidgetData, 'setting'> & {
-  setting?: { imgSrc?: string | null };
-};
 
 @Component({
   selector: 'lib-widget-img',
   templateUrl: './widget-img.component.html',
   styleUrls: ['./widget-img.component.scss'],
 })
-export class WidgetImgComponent extends BaseWidgetContent implements OnInit {
-  widgetData: ImgWidgetData = { setting: { imgSrc: '' } };
+export class WidgetImgComponent
+  extends BaseWidgetContent
+  implements OnInit, OnDestroy
+{
+  widgetData: WidgetData<ImgSetting> = { setting: { imgSrc: '' } };
 
   constructor() {
     super();
   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    if (this.widgetData.setting.imgSrc) {
+      URL.revokeObjectURL(this.widgetData.setting.imgSrc);
+    }
+  }
 }
