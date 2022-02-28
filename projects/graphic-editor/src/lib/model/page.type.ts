4f924add30d39d82f7397de3bd0684abbdc4503f
liyuhua <liyuhua@example.com>
2022-02-28 15:26:55 +0800

    update

--- projects/graphic-editor/src/lib/model/page.type.ts
+++ projects/graphic-editor/src/lib/model/page.type.ts
@@ -1,10 +1,8 @@
-import { ComponentRef } from '@angular/core';
 import { PageStyle, Widget, WidgetData, WidgetStyle } from '.';
-import { WidgetComponent } from '../widget-lib/widget/widget.component';
 
 export type Page = {
   style: PageStyle;
   widgets?: { type: string; style: WidgetStyle; widgetData?: WidgetData }[];
-  _widgetRefs?: ComponentRef<WidgetComponent>[];
+  // _widgetRefs?: ComponentRef<WidgetComponent>[];
   subPage?: Page[];
 };
